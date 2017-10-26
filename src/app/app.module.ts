7a59226fe2fcea0c9147aa89bca0abb16e849997
Nae <chanapat991@gmail.com>
2017-10-26 16:48:28 +0700

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { FormsModule } from '@angular/forms'
+import { GetphotoService } from './services/getphoto.service'
+import { HttpModule } from '@angular/http'
+import { RouterModule, Routes} from "@angular/router";
+import { BookComponent } from './components/book/book.component'
+
+const appRoutes:Routes = [
+  {path:"", component:UserComponent},
+  {path:"about", component:AboutusComponent},
+  {path:"book", component:BookComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent,
+    BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ GetphotoService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
